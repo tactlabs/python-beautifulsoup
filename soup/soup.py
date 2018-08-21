f1d25dd486a6cccd6b08b2a24e4961c89e422342
nirosha <niroshakannan17@gmail.com>
2018-08-21 12:02:24 +0530

    Soup

---
+++ soup/soup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+#from sympy.polys.polytools import quo
+
+'''
+Created on Aug 21, 2018
+
+Course work: 
+
+@author: 
+
+Source:
+    
+'''
+
+from bs4 import BeautifulSoup
+import requests
+import os, os.path
+
+
+url = "http://www.espn.com/college-sports/football/recruiting/databaseresults/_/sportid/24/class/2006/sort/school/starsfilter/GT/ratingfilter/GT/statuscommit/Commitments/statusuncommit/Uncommited"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+listings = []
+for rows in soup.find_all("tr"):
+    if ("oddrow" in rows["class"]) or ("evenrow" in rows["class"]):
+        
+        name = rows.find("div", class_="name").a.get_text()
+        
+        hometown = rows.find_all("td")[1].get_text()
+        
+        school = hometown[hometown.find(",")+4:]
+        
+        city = hometown[:hometown.find(",")+4]
+        
+        position = rows.find_all("td")[2].get_text()
+        
+        grade = rows.find_all("td")[4].get_text()
+        
+        listings.append([name, school, city, position, grade])
+
+print(listings)
